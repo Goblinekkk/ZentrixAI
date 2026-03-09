244e408a4ffc94210b8bcc234605260a043389f7
Goblinekkk <matejzeleny13@gmail.com>
2026-03-09 13:34:18 +0100

    Update API key check in script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ async function send() {
     appendMsg('user', text);
     input.value = '';
 
-    if (API_KEY.includes("")) {
+    if (API_KEY.includes("SEM VLOŽ API KLÍČ")) {
         appendMsg('ai', "Chyba: Chybí API klíč v script.js.");
         return;
     }
